037ebc3dd1b5981a18a42066a2ef0cbabd1cb5b1
Ritesh Arora <68393812+ritesh2909@users.noreply.github.com>
2023-11-17 17:56:17 +0530

    Update upload.js

--- routes/upload.js
+++ routes/upload.js
@@ -6,6 +6,7 @@ import {
   uploadNewFile,
   deleteUpload,
   getAllUploads,
+  getAllUploadsV2
 } from "../controllers/upload.js";
 
 const storage = memoryStorage();
@@ -14,5 +15,6 @@ const upload = multer();
 router.post("/upload", upload.single("image"), verifyToken, uploadNewFile);
 router.delete("/remove/:id", verifyToken, deleteUpload);
 router.get("/uploads", verifyToken, getAllUploads);
+router.get("/all-uploads",getAllUploadsV2);
 
 export default router;
